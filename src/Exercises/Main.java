c6fc2310b5dc25691a4b6076930662e5b623b3b8
PsicoPapero <simone.conzo@outlook.it>
2025-04-28 18:49:44 +0200

    Creation of new packages for each exercise

---
+++ src/Exercises/Main.java
@@ -0,0 +1,13 @@
+package Exercises;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Libro libro = new Libro("Harry Potter", "J.K", 584);
+		libro.stampaLibro();
+		
+
+	}
+
+}
